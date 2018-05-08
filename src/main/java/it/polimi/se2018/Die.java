4541995de0e814fa7c7b6759794c17f0f9afa338
FedericoGalati <federico.galati@mail.polimi.it>
2018-05-08 16:30:20 +0200

    Added randomize setting on Die and some method on Round.

--- src/main/java/it/polimi/se2018/Die.java
+++ src/main/java/it/polimi/se2018/Die.java
@@ -1,15 +1,25 @@
 package it.polimi.se2018;
 
+
+import java.util.Random;
+
+
+
 public class Die {
 
     private Colour colour;
     private int value;
+    private Random generator;
 
-    public Die(int n, Colour c){
+    public Die(Colour c){
         colour = c;
-        value = n;
+        value = -1;
+        generator = new Random();
     }
 
     public int getValue(){ return value;  }
     public Colour getColour(){ return colour;}
+    public void setValue(int n) {value = n;}
+    public void setRandomValue() { value = generator.nextInt(6) + 1;}
+
 }
--- src/main/java/it/polimi/se2018/Round.java
+++ src/main/java/it/polimi/se2018/Round.java
@@ -3,8 +3,16 @@ package it.polimi.se2018;
 public class Round {
 
     private DraftPool draftPool;
+    private Player playerTurn;
 
-    public Round() {
+    public Round(DraftPool dp, Player p) {
+
+        draftPool = dp;
+        playerTurn = p;
 
     }
+
+    public void setPlayerTurn(Player p) { playerTurn = p;}
+    public Player getPlayerTurn() { return playerTurn;}
+
 }
