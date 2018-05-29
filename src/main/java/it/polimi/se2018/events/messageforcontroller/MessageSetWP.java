79c3ed1ba2eebf8b3aa79685475424a5f2cfbe59
Gabriele Guelfi <gabriele.guelfi@mail.polimi.it>
2018-05-29 16:16:05 +0200

    split message for controller and message for view

---
+++ src/main/java/it/polimi/se2018/events/messageforcontroller/MessageSetWP.java
@@ -0,0 +1,31 @@
+package it.polimi.se2018.events.messageforcontroller;
+
+import it.polimi.se2018.controller.VisitorController;
+import it.polimi.se2018.events.Message;
+
+public class MessageSetWP extends Message {
+
+
+    private int firstIndex;
+    private int secondIndex;
+
+    public MessageSetWP(String nickname, int firstIndex, int secondIndex) {
+        super(nickname);
+        this.firstIndex = firstIndex;
+        this.secondIndex = secondIndex;
+    }
+
+    public int getFirstIndex() {
+        return firstIndex;
+    }
+
+    public int getSecondIndex() {
+        return secondIndex;
+    }
+
+    @Override
+    public void accept(VisitorController v) {
+        v.visit(this);
+    }
+
+}
